75a94f065a2fd634b5152087bd3257b78093b5b9
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-24 13:28:33 +0530

    added login class

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,43 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    private By usernameInput = By.id("username");
+    private By passwordInput = By.id("password");
+    private By loginButton = By.cssSelector("button[type='submit']");
+    private By flashMessage = By.id("flash");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void clickLogin() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginButton)).click();
+    }
+
+    public String getFlashMessage() {
+        WebElement message = wait.until(ExpectedConditions.visibilityOfElementLocated(flashMessage)
+        );
+
+        return message.getText();
+
+    }
+
+
+
+}
+
+
