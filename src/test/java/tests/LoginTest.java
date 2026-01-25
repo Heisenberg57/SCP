ad697c0d799690380da950396136e47f19da98e6
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-25 19:42:04 +0530

    added invalid login test

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import base.BaseTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.LoginPage;
@@ -8,6 +10,8 @@ import pages.SecureAreaPage;
 import utils.ConfigReader;
 
 public class LoginTest extends BaseTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
 
     @Test
     public void validLoginTest(){
@@ -26,7 +30,21 @@ public class LoginTest extends BaseTest {
         String message= loginPage.getFlashMessage();
         Assert.assertTrue(message.contains("secure area"),
                 "Success message not found: " + message);
+    }
 
+    @Test
+    public void invalidLoginTest(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open(baseUrl);
 
+        loginPage.enterUsername("wronguser");
+        loginPage.enterPassword("wrongpass");
+        loginPage.clickLogin();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("/login"),"User should remain on login page after invalid login");
+        Assert.assertTrue(loginPage.isErrorVisible(),"Error Message should be visible for invalid login");
+
+        String message = loginPage.getFlashMessage();
+        Assert.assertTrue(message.toLowerCase().contains("invalid"),"Expected invalid login message, got: " + message);
     }
 }
