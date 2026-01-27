9cc71118e86a8d5d8fc928932dfdf6040a4f3fdb
Suyog Nimbre <heisenbergnimbre@gmail.com>
2026-01-27 22:41:21 +0530

    Updated SecureAreaPage code

--- src/test/java/pages/SecureAreaPage.java
+++ src/test/java/pages/SecureAreaPage.java
@@ -4,32 +4,39 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.WaitUtils;
 
 import java.time.Duration;
 
 public class SecureAreaPage {
     private WebDriver driver;
-    private WebDriverWait wait;
+    //private WebDriverWait wait;
+    private WaitUtils waitUt;
 
     private By logoutButton = By.cssSelector("a.button.secondary.radius");
     private By flashMessage = By.id("flash");
 
     public SecureAreaPage(WebDriver driver){
         this.driver = driver;
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        //this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.waitUt = new WaitUtils(driver);
 
     }
 
     public boolean isLogoutButtonVisible(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(logoutButton)).isDisplayed();
+        //return wait.until(ExpectedConditions.visibilityOfElementLocated(logoutButton)).isDisplayed();
+        return waitUt.waitForVisibility(logoutButton).isDisplayed();
+
     }
 
     public void clickLogout(){
-        wait.until(ExpectedConditions.elementToBeClickable(logoutButton)).click();
+        //wait.until(ExpectedConditions.elementToBeClickable(logoutButton)).click();
+        waitUt.waitForClickability(logoutButton);
     }
 
     public String getLogoutMessage(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(flashMessage)).getText();
+        //return wait.until(ExpectedConditions.visibilityOfElementLocated(flashMessage)).getText();
+        return waitUt.waitForVisibility(flashMessage).getText();
     }
 
 }
